9f0acb2fd9cfce68127763e334d281b022d08ca1
samandera <ela.lach@gmail.com>
2017-05-05 22:49:20 +0200

    add font loader

--- src/components/FirstComponent.js
+++ src/components/FirstComponent.js
@@ -3,13 +3,8 @@ import React from 'react';
 class FirstComponent extends React.Component {
   render() {
     return (
-      <div>
-        <button className="first-component" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-          Your first Component
-        </button>
-        <ul className="dropdown-menu" aria-labelledby="dropdownMenu1">
-          <li>You know you are using bootstrap, right?</li>
-        </ul>
+      <div className="first-component">
+        Your first Component
       </div>
     );
   }
